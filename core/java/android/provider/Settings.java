180147a0d2dc52d09ecf27da18c0b402f684ee4e
DvTonder <david.vantonder@gmail.com>
2013-11-19 12:15:32 +0530

    [2/2] frameworks/base: Enable quick unlock
    Automatically unlock the lockscreen when the correct pin/password is keyed.
    
    Settings: https://gerrit.omnirom.org/#/c/2605/
    
    Change-Id: I3c1df4b573321a9343d1c339055577d5ca737f14

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2509,6 +2509,12 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
