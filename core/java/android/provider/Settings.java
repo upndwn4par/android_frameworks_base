02f80fee5773cad28b493ff2b67e8bab8237d332
BigBrother1984 <stevewatersy@gmail.com>
2014-06-06 20:03:16 +0200

    HOVER [1/2]
    Notifications refined:
    The way you handle with the incoming notifications defines how much you enjoy your device.
    Hover shows you those notifications when you need them in a beautiful card that briefly appears
    on your screen. For a detailed look of the notification, you can swipe down to expand some more
    info, or you can tap it to open the related app using PA’s floating mode.
    
    Introducing Hover:
    https://plus.google.com/107979589566958860409/posts/DC4KyDd1cDC
    https://plus.google.com/107979589566958860409/posts/BtWoGpNd8Fq
    
    This is revision 4 of the original base by @D4rKn3sSyS . Hover works by moving the
    notification view from statusBar container to Hover’s one and re-themes notifications
    in the process. When hover is active it is the owner of the view, special care must
    be taken to not  manipulate the statusbar reference while hoveris active.
    Hover’s view state is managed by a handler based timer. When any touch event is received
    or a gesture completed the timer is restarted, upon expiration of the timer hover is
    hidden or will show next notification in the list if there is one (or more) to show.
    The timer along with animation listeners  regulates the rate at which Hover can process new
    notifications, acting as a spam filter for noisy applications. Upon first run a dialog is
    displayed explaining Hover to new users. More infos in code’s documentation.
    
    Todo:
    - Swipe up to dismiss only the hover window not the notification in status bar.
    - Transparent support has been tweaked a bit but needs a more proper solution
    - Edge gap fix for Theme Engine themes overriding notifications draws
    - Long press action(s)
    - MInor tweaks noted in the code’s comments
    
    CONTRIBUTORS:
    @D4rKn3sSyS
    @BigBrother1984
    @Anu6is
    @beerbong
    @aaronpoweruser
    @David96
    @linuxman94
    
    Drawables by @arzbhatia
    
    Signed-off-by: Jesus David <jesdga95@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Signed-off-by: Curtis Mayers <curtis.mayers@gmail.com>
    Signed-off-by: Yamil Ghazi <stevewatersy@gmail.com>
    Signed-off-by: Aaron Gascoigne <atlrudeboi1@gmail.com>
    Signed-off-by: David L.-W. <david.leppla.weber@gmail.com>
    Signed-off-by: Evan Anderson <evan@thelinuxman.us>
    Conflicts:
    	packages/SystemUI/res/values/pa_dimens.xml
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationHelper.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/input/InputWindow.cpp

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2014, ParanoidAndroid Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -2862,6 +2863,14 @@ public final class Settings {
         public static final String NOTIFICATION_BACKGROUND_ALPHA = "notification_background_alpha";
 
         /**
+         * Hover, default is 0 (off).
+         * 0 = disabled
+         * 1 = enabled
+         * @hide
+         */
+        public static final String HOVER_STATE = "hover_state";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4839,6 +4848,9 @@ public final class Settings {
 
         /** @hide */
         public static final String IMMERSIVE_MODE_CONFIRMATIONS = "immersive_mode_confirmations";
+
+        /** @hide */
+        public static final String HOVER_FIRST_TIME = "hover_first_time";
 
         /**
          * This is the query URI for finding a print service to install.
