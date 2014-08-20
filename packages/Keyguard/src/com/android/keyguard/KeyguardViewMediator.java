0a07aeeb9cd4774350ff1b04c20221cb202e95d8
upndwn4par <upndwn4par@gmail.com>
2014-08-19 23:34:44 -0400

    Disable screen lock/unlock sound if music is playing

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -1203,6 +1203,9 @@ public class KeyguardViewMediator {
             // If the stream is muted, don't play the sound
             if (mAudioManager.isStreamMute(mMasterStreamType)) return;
 
+	    // Disable screen lock/unlock sound if music is playing
+	    if (mAudioManager.isMusicActive()) return;
+
             mLockSoundStreamId = mLockSounds.play(whichSound,
                     mLockSoundVolume, mLockSoundVolume, 1/*priortiy*/, 0/*loop*/, 1.0f/*rate*/);
         }
