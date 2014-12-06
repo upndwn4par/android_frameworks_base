0091b8dde61ebe2afd8dd48a5fc6da74f03b4b52
upndwn4par <upndwn4par@gmail.com>
2014-12-05 19:04:37 -0500

    [1/2] Nav bar height/width customization Based on this commit: https://github.com/OmniKang/android_frameworks_base/commit/e16f12b553a872e400a2ce8754255f75ead68b14
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I420beb7800098e056b52b9b82123e8d0d69e3839
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/navigation_bar.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3535,6 +3535,18 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
